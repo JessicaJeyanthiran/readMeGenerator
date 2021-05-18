4c49acede6d9d55cb7e8926ae29dadbffd3b5dd0
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2021-05-18 18:43:10 -0400

    install inquire

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# readMeGenerator
+# j
+
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // TODO: Include packages needed for this application
+const inquire = require ("inquirer");
+const generateMarkdown = require ("./utils/generateMarkdown");
+console.log(generateMarkdown);
+const fs = require ("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
